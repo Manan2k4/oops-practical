414a8d3d51a43a81ed600ed97448b1444d18e34b
Jagdish2401 <149369569+Jagdish2401@users.noreply.github.com>
2024-02-09 13:13:23 +0530

    Update practical4.cpp

--- practical4.cpp
+++ practical4.cpp
@@ -1,1 +1,42 @@
+/*A system calculates the area of circle, rectangle and cuboid. Every time it called the same function for
+calculating the area. Depending on the number of parameters passed, it chooses where to find area or
+circle / rectangle / cuboid. */
+
+#include<iostream>
+using namespace std;
+#define pi 3.14
+
+float area(int radius)
+{
+    return pi*radius*radius;
+}
+int area(int width,int length)
+{
+    return width*length;
+}
+int area(int length,int width,int height)
+{
+    return 2*(length*width + width*height + height*length  );
+}
+int main()
+{
+    float radius,length,height,width;
+    cout<<"Enter the radius of a circle:"<<endl;
+    cin>>radius;
+    cout<<"The area of a circle is:"<<area(radius)<<endl;
+    cout<<"Enter the lenghth of a rectangle:";
+    cin>>length;
+    cout<<"Enter the width of a rectangle:";
+    cin>>width;
+    cout<<"the area of a rectangle is:"<<area(length,width)<<endl;
+    cout<<"Enter the lenghth of a cuboid:";
+    cin>>length;
+    cout<<"Enter the height of a cuboid:";
+    cin>>height;
+     cout<<"Enter the width of a cuboid:";
+     cin>>width;
+    cout<<" the area of a cuboid:"<<area(length,width,height);
+    cout<<"\n \n JAGDISH 23CS025";
+    return 0;
+}
 
